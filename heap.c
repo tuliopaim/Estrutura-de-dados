3ff7e81a45da64b28cd2b161f3177ffed6a7ddc7
tuliopaim <tutpaim@gmail.com>
2018-05-10 18:28:32 -0300

    heap_sort() implementado

--- heap.c
+++ heap.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAIOR(a,b) (a > b) ? a : b;
 
 /////////////////////////////////////////////////////////
 //ESTRUTURA HEAP
+/////////////////////////////////////////////////////////
 
 typedef struct HEAP{
   int size;
@@ -12,65 +12,13 @@ typedef struct HEAP{
 }heap;
 
 
-
 /////////////////////////////////////////////////////////
 //CARDAPIO
+/////////////////////////////////////////////////////////
 
-int compare(int a, int b);
-
+/////////////////////////////////////////////////////////
 heap * heap_start(int size);
-int get_parent(int i);
-void heap_up(heap * h, int i);
-void heap_down(heap * h, int i);
-void heap_insert(heap * h, int value);
-void heap_remove(heap * h, int value);
-int get_index_of(heap * h, int value);
-
-void heap_print(heap * h, int i, int lvl);
-void heap_sort(int * numeros, int size);
-
-
 /////////////////////////////////////////////////////////
-//MAIN
-/////////////////////////////////////////////////////////
-
-int main(){
-
-  int temp = 1, size = 10;
-
-  heap * h = heap_start(size);
-
-  while(1){
-    scanf("%d", &temp);
-    if(temp == 0) break;
-    heap_insert(h,temp);
-    printf("\n\n");
-    heap_print(h,0,0);
-  }
-
-  printf("Remover: \n" );
-
-  while(1){
-    scanf("%d\n", &temp);
-    if(temp == 0) break;
-    heap_remove(h,temp);
-    printf("\n\n");
-    heap_print(h,0,0);
-  }
-
-
-}
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////
-//FUNÇÕES
-
-int compare(int a, int b){
-  return (a > b) ? 1 : 0;
-}
-
 
 heap * heap_start(int size){
   heap * h = malloc(sizeof(heap));
@@ -81,110 +29,165 @@ heap * heap_start(int size){
 }
 
 
-int get_parent(int i){
-  return (i-1)/2;
-}
-
-
-void heap_insert(heap * h, int value){
-
-  if(h->length >= h->size) return;
-
-  h->values[h->length] = value;
-
-  heap_up(h, h->length++);
-
-}
+/////////////////////////////////////////////////////////
+void heap_up(heap * h, int i);
+/////////////////////////////////////////////////////////
 
 void heap_up(heap * h, int i){
-  int parent = get_parent(i);
-
-  while( i > 0 && (h->values[i] > h->values[parent]) ){
-    //SWAP
-    int temp = h->values[i];
-    h->values[i] = h->values[parent];
-    h->values[parent] = temp;
+  int parent = (i-1)/2;
+  while((parent >= 0) && h->values[parent] < h->values[i]){
+    int temp = h->values[parent];
+    h->values[parent] = h->values[i];
+    h->values[i] = temp;
 
     i = parent;
-    parent = get_parent(i);
-  }
-}
-
-int maior(int a, int b){
-  return (a > b) ? a : b;
-}
-
-void heap_down(heap * h, int pai){
-  int filho;
-  if(h->values[2*pai+1] > h->values[2*pai+2])
-    filho = 2*pai+1;
-  else
-    filho = 2*pai+2;
-
-  while(filho <= h->length-1 && (h->values[pai] < h->values[filho])){
-
-    //SWAP
-    int temp = h->values[pai];
-    h->values[pai] = h->values[filho];
-    h->values[filho] = temp;
-
-    pai = filho;
-
-    if(h->values[2*pai+1] > h->values[2*pai+2])
-      filho = 2*pai+1;
-    else
-      filho = 2*pai+2;
+    parent = (i-1)/2;
   }
 }
 
 
-void heap_remove(heap * h, int value){
-  int i = get_index_of(h, value);
-  int valor = h->values[i];
-  h->values[i] = h->values[h->length-1];
-  h->values[h->length-1] = -1;
-  h->length--;
+/////////////////////////////////////////////////////////
+void heap_down(heap * h, int i);
+/////////////////////////////////////////////////////////
 
-  printf("\n\nRemovido: %d\n", valor);
+void heap_down(heap * h, int i){
+  int l = 2*i+1;
+  int r = 2*i+2;
+  int maior;
 
-  heap_down(h, i);
-
-  heap_up(h, i);
-
+  if(l >= h->length) return; // SE NÃO HOUVER FILHOS
+  //FILHO DA ESQUERDA
+  if(h->values[l] > h->values[i]) maior = l;
+  else maior = i;
+  //FILHO DA DIREITA SE HOUVER
+  if(r < h->length && h->values[r] > h->values[maior]) maior = r;
+  //SWAP
+  if(i!=maior){
+    int temp = h->values[maior];
+    h->values[maior] = h->values[i];
+    h->values[i] = temp;
+    heap_down(h,maior);
+  }
 }
 
+
+/////////////////////////////////////////////////////////
+void heap_insert(heap * h, int value);
+/////////////////////////////////////////////////////////
+
+void heap_insert(heap * h, int value){
+  if(h->length>=h->size) return ;
+  h->values[h->length] = value;
+  heap_up(h, h->length++);
+}
+
+/////////////////////////////////////////////////////////
+int get_index_of(heap * h, int value);
+/////////////////////////////////////////////////////////
 
 int get_index_of(heap * h, int value){
-  for(int i = 0; i < h->length; i++)
+  for(int i = 0; i < h->length; i++){
     if(h->values[i] == value) return i;
+  }
 }
 
+
+/////////////////////////////////////////////////////////
+void heap_remove(heap * h, int value);
+/////////////////////////////////////////////////////////
+
+void heap_remove(heap * h, int value){
+  int i = get_index_of(h,value);
+  int temp = h->values[i];
+  h->values[i] = h->values[--h->length];
+  h->values[h->length] = temp;
+
+  heap_down(h,i);
+  heap_up(h,i);
+}
+
+/////////////////////////////////////////////////////////
+void heap_print(heap * h, int i, int lvl);
+/////////////////////////////////////////////////////////
 
 void heap_print(heap * h, int i, int lvl){
-    if(i < h->length){
-      //left
-      heap_print(h,2*i + 1, lvl+1);
-      //printf
-      for(int i = 0; i < lvl; i++) printf("-- ");
-      printf("%d\n",h->values[i]);
-      //right
-      heap_print(h,2*i+2,lvl+1);
-    }
+  if(i < h->length){
+    heap_print(h, 2*i+2,lvl+1);
+
+    for(int i = 0; i < lvl; i++) printf((i==0) ? "  " : "-- ");
+    printf("%d\n", h->values[i]);
+
+    heap_print(h, 2*i+1,lvl+1);
+  }
 }
 
 
-void heap_sort(int * numeros, int size){
-  heap * h = heap_start(size);
-  for(int i = 0; i < size; i++){
-    heap_insert(h,numeros[i]);
+/////////////////////////////////////////////////////////
+void heap_sort(heap * h, int size);
+/////////////////////////////////////////////////////////
+
+void heap_sort(heap * h, int size){
+  for (int i = (size/2)-1; i >= 0 ; i--){
+    heap_down(h, i);
   }
-  for(int i = 0; i < size; i++){
-    //printf("\n\n");
-    //heap_print(h,0,0);
-    int valor = h->values[0];
-    h->values[0] = h->values[h->length-1];
-    h->values[--(h->length)] = -1;
+
+  while(h->length-- > 1){
+    int temp = h->values[h->length];
+    h->values[h->length] = h->values[0];
+    h->values[0] = temp;
     heap_down(h,0);
-    printf((h->length==0) ? "%d\n" : "%d ",valor);
+    // PASSO A PASSO
+    // for(int i = 0; i< size; i++) printf("%d ", h->values[i]);
+    // printf("\n" );
   }
+
+
+
+
+}
+
+
+
+
+/////////////////////////////////////////////////////////
+//MAIN
+/////////////////////////////////////////////////////////
+
+int main(){
+
+  int temp = 1, size = 10, i = 0;
+  int numeros[size];
+  heap * h = heap_start(size);
+  while(1){
+    scanf("%d", &temp);
+    if(temp == 0) break;
+    // heap_insert(h,temp);
+    // printf("\n\n");
+    // heap_print(h,0,0);
+    h->values[h->length++] = temp;
+  }
+
+  //heap_print(h,0,0);
+
+  printf("Numeros[] = ");
+  for (size_t j = 0; j < size; j++) {
+    printf("%d ", h->values[j]);
+  }printf("\n");
+
+  heap_sort(h, size);
+
+  printf("Numeros[] = ");
+  for (size_t j = 0; j < size; j++) {
+    printf("%d ", h->values[j]);
+  }printf("\n");
+
+  // printf("Remover: \n" );
+  //
+  // while(1){
+  //   scanf("%d\n", &temp);
+  //   if(temp == 0) break;
+  //   heap_remove(h,temp);
+  //   printf("\n\n");
+  //   heap_print(h,0,0);
+  // }
 }
--- union_find/unionFind.c
+++ union_find/unionFind.c
@@ -1,106 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 10
 
-typedef struct NODE{
-  int value;
-  struct NODE * root;
-  struct NODE * prox;
-}node;
+typedef struct Set{
+  int size;
+  int * numeros;
+  int * pai;
+}set;
 
-typedef node * set;
+set * new_set(int * entradas, int size){
+  int maior = 0;
+  for (int i = 0; i< size; i++) if(entradas[i] > size) size = entradas[i];
 
-set make_set(int value);
-int set_size(set a);
-node * set_union(set a, set b);
-void set_print(set a);
-set find(set a);
+  set * new = (set*)malloc(sizeof(set));
+
+  new->size = size;
+  new->numeros = (int)malloc(sizeof(int)*size);
+  new->pai = (int)malloc(sizeof(int)*maior);
+  return new;
+}
+
+void make_set(set * s){
+  for(int i = 0; i < s->size; i++){
+    s->parent[numeros[i]] = 0;
+  }
+}
 
 
 int main(){
+  int entradas[SIZE];
 
-  int temp;
+  for(int i = 0; i < 10; i++){
+    scanf("%d", &temp);
+    entradas[i] = temp;
+  }
 
-  set a = make_set(1);
-  set b = make_set(2);
-  set c = make_set(3);
-  set d = make_set(4);
-  set e = make_set(5);
-
-  set_print(a);
-  set_print(b);
-  set_print(c);
-  set_print(d);
-  set_print(e);
-
-  //printf("%d %d\n", set_size(a), set_size(b));
-  set ab = set_union(a,b);
-  set_print(ab);
-  set ac = set_union(ab,c);
-  set_print(ac);
-  set bd = set_union(b,d);
-  set_print(bd);
-  set be = set_union(b,e);
-  set_print(be);
-  //set_print(b);
-
+  set * s = new_set();
+  make_set(s);
 
   return 0;
-}
-
-set make_set(int value){
-  set new = (node*)malloc(sizeof(node));
-  new->value = value;
-  new->root = new;
-  new->prox = NULL;
-  return new;
-}
-
-int set_size(set a){
-  int count = 0;
-  set temp = a;
-  while(temp!=NULL){
-    count++;
-    temp = temp->prox;
-  }
-  return count;
-}
-
-node * set_union(set a, set b){
-  set new, temp1, temp2;
-  temp1 = find(a);
-  temp2 = find(b);
-  if(set_size(temp1) >= set_size(temp2)){
-    new = temp1;
-    temp1 = a;
-    temp2 = b;
-  }else{
-    new = temp2;
-    temp1 = b;
-    temp2 = a;
-  }
-  while(temp1->prox!=NULL){
-    temp1 = temp1->prox;
-  }
-
-  temp1->prox = temp2;
-
-  while(temp1=NULL){
-    temp1->root = new;
-    temp1 = temp1->prox;
-  }
-  return new;
-}
-
-void set_print(set a){
-  set temp = a;
-  printf("[%d]->", temp->value);
-  temp = temp->prox;
-  while (temp!=NULL) {
-    printf("%d->", temp->value);
-    temp = temp->prox;
-  }printf("NULL\n");
-}
-
-set find(set a){
-  return a->root;
 }
